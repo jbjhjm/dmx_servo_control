ae4ff3cd7e8e55621f4dea77bca15eea42b8d3ba
JANNIK-NOTEBOOK\Jannik <jm@wolfinteractive.de>
2020-11-17 22:01:54 +0100

    WIP - test + fix code

--- src/main.cpp
+++ src/main.cpp
@@ -3,51 +3,84 @@
 #include <DMXSerial.h>
 
 const int DmxTimeoutMs = 		5000;
-const int DmxDipPins[9] = 		{ 3,4,5,6,7,8,9,10,11 };
+const int DmxDipPins[9] = 		{ 2,3,4,5,6,7,8,9,10 };
 
 Servo Servo1; 
-const int Servo1CtrlPin = 		9;
+const int Servo1CtrlPin = 		13;
 const int Servo1DefaultPos =	0;
 const int Servo1MinPos = 		0;
-const int Servo1MaxPos = 		200;
+const int Servo1MaxPos = 		90;
+
+//  Can only use either Serial or DMXSerial!
 
 void setup() {
+//   Serial.begin(9600);
   DMXSerial.init(DMXReceiver);
-  DMXSerial.write(Servo1CtrlPin, Servo1DefaultPos);
+  // DMXSerial.write(Servo1CtrlPin, Servo1DefaultPos);
 
   pinMode(Servo1CtrlPin, OUTPUT);	
-  Servo1.attach(Servo1CtrlPin);
+  Servo1.attach(Servo1CtrlPin,500,2500);
+
+  pinMode(LED_BUILTIN, OUTPUT);
 
   for(int i = 0; i<9; i++){
-    pinMode(DmxDipPins[i], INPUT);      // set the digital pins (defined above) as input
-    digitalWrite(DmxDipPins[i], HIGH);  // set internal pullup resistor on
-  }
-}
-
-void loop() {
-  // Calculate how long no data backet was received
-  unsigned long lastPacket = DMXSerial.noDataSince();
-  uint16_t DmxAddress = getDipAddress();
-  
-  if (lastPacket < DmxTimeoutMs) {
-	uint8_t value = DMXSerial.read(DmxAddress);
-	// option1: map DMX value range to range  Servo1MinPos Servo1MaxPos
-	// value = map(value, 0, 255, Servo1MinPos, Servo1MaxPos);
-	// option2: don't map to range, just jump between Min and Max Pos
-	value = (value>=128) ? Servo1MaxPos : Servo1MinPos;
-  	Servo1.write(value);
-  } else {
-    // Send default value if no package has been received for 5+ seconds
-  	Servo1.write(Servo1DefaultPos);
+    pinMode(DmxDipPins[i], INPUT_PULLUP); // set the digital pins (defined above) as input
   }
 }
 
 // cannot use uint8 as DMX uses 9 bits
 uint16_t getDipAddress(){
   int i,address=0;
-  for(i=0; i<9; i++){
+  for(i=1; i<=9; i++){
 	  // build address number by left shifting by 1, then merge next bit by OR operation
-    address = (address << 1) | digitalRead(DmxDipPins[i]);
+	  // start from last input which will be the first bit in built int!
+    address = (address << 1) | !digitalRead(DmxDipPins[9-i]);
   }
   return address;
+}
+
+// Caution: bitOffset starts from 0!
+bool getBit(byte data, int bitOffset = 0) {
+	// Create a mask by shifting (<<) "1" bit to the left.
+	// then use AND (&) to check if the bit exists in both values.
+    return (data & (1<<bitOffset)) != 0;
+}
+
+void printDipValues() {
+//   Serial.print(digitalRead(DmxDipPins[0]));
+//   Serial.print(digitalRead(DmxDipPins[1]));
+//   Serial.print(digitalRead(DmxDipPins[2]));
+//   Serial.print(digitalRead(DmxDipPins[3]));
+//   Serial.print(digitalRead(DmxDipPins[4]));
+//   Serial.print(digitalRead(DmxDipPins[5]));
+//   Serial.print(digitalRead(DmxDipPins[6]));
+//   Serial.print(digitalRead(DmxDipPins[7]));
+//   Serial.print(digitalRead(DmxDipPins[8]));
+//   Serial.print(" (");
+//   Serial.print(getDipAddress());
+//   Serial.print(")");
+//   Serial.print("\r\n");
+}
+
+void loop() {
+  // // Calculate how long no data packet was received
+  unsigned long lastPacket = DMXSerial.noDataSince();
+  uint16_t DmxAddress = getDipAddress();
+//   printDipValues();
+  
+  Servo1.write(Servo1MaxPos);   
+  
+  if (lastPacket < DmxTimeoutMs) {
+    uint8_t value = DMXSerial.read(DmxAddress);
+    // option1: map DMX value range to range  Servo1MinPos Servo1MaxPos
+    // value = map(value, 0, 255, Servo1MinPos, Servo1MaxPos);
+    // option2: don't map to range, just jump between Min and Max Pos
+    value = (value>=128) ? Servo1MaxPos : Servo1MinPos;
+  	// Servo1.write(value);
+    digitalWrite(LED_BUILTIN, HIGH);
+  } else {
+    // Send default value if no package has been received for 5+ seconds
+  	Servo1.write(Servo1DefaultPos);
+    digitalWrite(LED_BUILTIN, LOW);
+  }
 }
